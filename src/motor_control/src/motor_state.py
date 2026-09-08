5be7df25830989a5151cbfa4a39e49c85327d855
DanielHaikal05 <cyberarc28@gmail.com>
2026-09-08 16:03:11 -0400

    State estimator fix

--- src/motor_control/src/motor_state.py
+++ src/motor_control/src/motor_state.py
@@ -53,15 +53,11 @@ class Motor_state(Node):
 
         self.timeout = 2
 
-        self.t_prev = np.array([[None, None, None],
-                                [None, None, None],
-                                [None, None, None],
-                                [None, None, None]], dtype=object)
+        self.edge_count = np.zeros((3,2), dtype=int)
+        self.edges = np.zeros((3,25), dtype=int)
+        
+        self.t = np.ones((3,25), dtype=object) * monotonic()
 
-        self.is_valid = np.array([[False, False, False],
-                                  [False, False, False],
-                                  [False, False, False],
-                                  [False, False, False]])
 
         e11.when_activated   = lambda: self.read_encoder(0, 0, 1)
         e11.when_deactivated = lambda: self.read_encoder(0, 0, 0)
@@ -80,11 +76,10 @@ class Motor_state(Node):
 
         e32.when_activated   = lambda: self.read_encoder(2, 1, 1)
         e32.when_deactivated = lambda: self.read_encoder(2, 1, 0)
-
-        self.edge_count = np.zeros((3,2), dtype=int)
     
     
     def read_encoder(self, m, c, s):
+        t = monotonic()
         self.edge_count[m][c] +=1
         if self.encoders[m][c] == s:
             self.get_logger().warning("Called read_encoder with no change")
@@ -92,44 +87,24 @@ class Motor_state(Node):
         
         encoder_reading = self.encoders.copy()
         encoder_reading[m][c] = s
-        t = monotonic()
 
         direction = encoder_direction(self.encoders[m], encoder_reading[m])
-        if direction == 0:
-            self.get_logger().error(f"Invalid transition: {self.encoders[m]} --> {encoder_reading[m]}")
+        if direction == 0: 
+            self.get_logger().error(f"Invalid transition: {self.encoders[m]} --> {encoder_reading[m]}") 
             return
-        
-        if direction != self.direction[m]:
-            self.x[m,1:] = np.array([0,0])
-            self.x_bar[:,m,1:] = np.array([[0,0], [0,0], [0,0], [0,0]])
-            self.direction[m] = direction
-            self.t_prev[:,m] = None
-            self.is_valid[:,m] = False  
+
         self.encoders[m] = encoder_reading[m]
 
-        a = encoder_reading[m][0]; b = encoder_reading[m][1]
-        edge_index = 3*a + b - 2*a*b
+        self.edges[m,:] = np.concatenate((self.edges[m,1:], [self.edges[m,-1] + direction]))
+        dt = t - self.t[m,-1]
+        self.t[m,:] = np.concatenate((self.t[m,1:], [t]))
 
-        if self.t_prev[edge_index][m] is None:
-            self.t_prev[edge_index][m] = t
-            return
+        dx = self.edges[m,-1] - self.edges[m,0]
+        v = self.x[m][1]
 
-        dt = t - self.t_prev[edge_index][m]
-        self.t_prev[edge_index][m] = t
-        qdi = self.x_bar[edge_index][m][1]
-
-        self.x_bar[edge_index][m][0] += self.direction[m] * (2*pi/11) * (1/GEAR_RATIO)
-        self.x_bar[edge_index][m][1] = self.direction[m] * (2*pi/11) * (1/dt) * (1/GEAR_RATIO)
-        self.x_bar[edge_index][m][2] = (self.x_bar[edge_index][m][1] - qdi) / dt
-        self.is_valid[edge_index][m] = True
-
-        if edge_index == 0:
-            self.x[m,0] = self.x_bar[edge_index][m][0]
-
-        valid = self.is_valid[:,m]
-        self.x[m,1:] = np.zeros_like(self.x[m,1:])
-        if np.any(valid):
-            self.x[m,1:] = np.mean(self.x_bar[valid,m,1:], axis=0)
+        self.x[m][0] = self.edges[m,-1] * (2*pi/11) * (1/GEAR_RATIO) * (1/4)
+        self.x[m][1] = (2*pi/11) * (dx/dt) * (1/GEAR_RATIO) * (1/4)
+        self.x[m][2] = (self.x[m][1] - v) / dt
 
 
     def publish_joint_state(self):
@@ -146,16 +121,13 @@ class Motor_state(Node):
         msg2.data = self.edge_count.flatten()
         self.edge_pub.publish(msg2)
 
+
     def detect_timeout(self):
         t = monotonic()
-        for i in range(3):
-            if any(a is None for a in self.t_prev[:,i]): 
-                continue
-            if np.all(t > self.t_prev[:,i] + self.timeout):
-                self.x_bar[:,i,1:] = np.array([[0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0]])
-                self.x[i,1:] = np.array([0.0, 0.0])
-                self.t_prev[:,i] = np.array([None, None, None, None])
-                self.is_valid[:, i] = False
+        for m in range(3):
+            if t - self.t[m, -1] > self.timeout:
+                self.x[m, 1] = 0.0
+                self.x[m, 2] = 0.0
  
       
 def main(args=None):
